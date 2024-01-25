d559dd3729201fde1ff3538833173eee76a114da
Nick Sprague <n.sprague18@gmail.com>
2024-01-25 15:02:01 -0500

    added to html, began style.css, started script.js

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+var quizQuestionEl = document.getElementById('#quizQuestions-Container')
+var mainQuizContainer = document.getElementById('#quizMain-Container')
+
+//page buttons
+var buttonStartQuiz = document.querySelector('#start-coding-quiz')
