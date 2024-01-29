e565ca57526f00eda3af602288162a5700a316a5
Nick Sprague <n.sprague18@gmail.com>
2024-01-28 22:49:58 -0500

    added to index.html, and event listener in script.js, working on buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,34 @@
-var quizQuestionEl = document.getElementById('#quizQuestions-Container')
-var mainQuizContainer = document.getElementById('#quizMain-Container')
+var quizQuestionEl = document.getElementById('#quizQuestions-Container');
+var mainQuizContainer = document.getElementById('#quizMain-Container');
+var containerHighScoresEl = document.getElementById('#container-high-score');
+var viewHighScore = document.getElementById('#viewHighScores');
 
 //page buttons
-var buttonStartQuiz = document.querySelector('#start-coding-quiz')
+var buttonStartQuizEl = document.querySelector('#start');
+var backButton = document.querySelector('#back');
+var clearHighScores = document.querySelector('#clear')
+
+//to start game
+var startQuiz = function() {
+
+}
+
+//stored high scores
+var highScoresScreen = function () {
+    var StoredHighScores = localStorage.getItem('HighScores')
+    if (!StoredHighScores) {
+        return false;
+    }
+}
+
+var displayHighScores = function () {
+
+}
+
+HighScores = [];
+
+// button to start quiz
+buttonStartQuizEl.addEventListener('click', startQuiz)
+
+// event listener for view high scores
+highScore.addEventListener('click', displayHighScores)
